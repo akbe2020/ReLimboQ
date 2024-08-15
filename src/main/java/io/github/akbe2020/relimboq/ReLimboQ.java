b0865d5048f07191fea6ff719cf09fd24f3ae788
four4tReS <98309267+akbe2020@users.noreply.github.com>
2024-08-15 10:31:58 +0500

    Bump version to 0.1.0

--- src/main/java/io/github/akbe2020/relimboq/ReLimboQ.java
+++ src/main/java/io/github/akbe2020/relimboq/ReLimboQ.java
@@ -60,7 +60,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Plugin(
         id = "relimboq",
         name = "ReLimboQ",
-        version = "0.0.2",
+        version = "0.1.0",
         authors = {
                 "four4tReS"
         },
